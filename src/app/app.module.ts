bd21e0ad61807a60977fe42b9e1dabb3ffef8867
Oluwaseyi Bakare <oluwaseyi.bakare@gmail.com>
2020-02-01 20:10:43 +0100

    Customer Details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerDetailComponent } from './customer/customer-detail/customer-detail.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { CustomerItemComponent } from './customer/customer-list/customer-item/customer-item.component';
+import { CustomerStartComponent } from './customer/customer-start/customer-start.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomerService } from './customer/customer.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CustomerComponent,
+    CustomerDetailComponent,
+    CustomerListComponent,
+    CustomerItemComponent,
+    CustomerStartComponent,
+    CustomerEditComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [CustomerService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
